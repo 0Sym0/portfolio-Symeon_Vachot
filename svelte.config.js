2dfb6042dc7089f138a92c190b6fdfb54f1abcb1
root <root@HUBERT.>
2025-09-10 16:30:48 +0200

    test2

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
-const base = dev ? '' : '/porfolio';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',   // HTML + assets
+			assets: 'build',  // idem
+			fallback: '404.html'
+		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio-Symeon_Vachot' : ''
+			base: '/portfolio-Symeon_Vachot'
 		}
 	}
 };
+
 export default config;
